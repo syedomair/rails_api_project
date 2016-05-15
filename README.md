06523a2aaba604f58a98106d91ebcf08ee66b971
syedomair <syedkhalidomair@gmail.com>
2016-05-15 17:17:23 +0500

    clean up routes

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # rails_api_project
+
+to run spec
+bundle exec rspec spec/controllers/api/v1/catalogs_controller_spec.rb 
+
+On Browser
+http://localhost:3000/api/catalogs/1
+returns
+{"id":1,"name":"Test Catalog Name"}
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
 
   RailsApiProject::Application.routes.draw do
-  # Api definition
-  #  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
-  #    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
   # We are going to list our resources here
     namespace :api do
       scope module: :v1 do
-        resources :users, :only => [:show, :create] 
-        resources :sessions, :only => [:create, :destroy]
-        #do 
-        #end
         resources :catalogs, :only => [:show, :create] 
       end
     end
